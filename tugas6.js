3c01c4d5b75f624898a2bd542b5add7554b8a1ed
susila-tropika <susilatropikanew@gmail.com>
2021-08-29 03:52:38 +0700

    menambahkan file tugas6

---
+++ tugas6.js
@@ -0,0 +1,12 @@
+function bilangan(){
+    for (let b=0 ; b<=20 ; b++){
+        if (b%2==0){
+            console.log("Bilangan Genap")
+        }
+        else{
+            console.log(b)
+        }
+     
+    }
+}
+bilangan()
